d5beb0e7d15213f3f495f593a0bd37816a1ac09b
unknown <thcodbs@naver.com>
2024-05-08 02:30:24 +0900

    로그인 시도

--- blog/src/layouts/Header.js
+++ blog/src/layouts/Header.js
@@ -5,14 +5,39 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import valanse_logo from "./img/valanse_logo.png";
 import SignUpmodel from "../modal/SignUpmodel";
+import axios from 'axios'; // Axios 라이브러리 추가
 import { Link } from 'react-router-dom';
 
-
 const Header = () => {
-    const [SignUpmodelOn, setSignUpModalOn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 관리하는 상태 변수
+    const [showSignUpModal, setShowSignUpModal] = useState(false); // 회원가입 모달의 표시 여부를 관리하는 상태
+
+    // 로그인 상태에 따라 로그인 또는 로그아웃 버튼을 토글하는 함수
+    const toggleLoginButton = () => {
+        setIsLoggedIn(!isLoggedIn);
+    };
+
+    // 로그아웃 함수
+    const handleLogout = async () => {
+        try {
+            // 로그아웃 요청 보내기
+            await axios.post('http://54.180.170.88:8080/token/logout');
+
+            // 로그아웃 성공 시 상태 변경
+            setIsLoggedIn(false);
+        } catch (error) {
+            console.error('로그아웃 중 에러 발생:', error);
+        }
+    };
+
+    // 로그인 모달을 열거나 닫는 함수
+    const toggleSignUpModal = () => {
+        setShowSignUpModal(!showSignUpModal);
+    };
+
     return (
         <>
-            <SignUpmodel show={SignUpmodelOn} onHide={() => setSignUpModalOn(false)} />
+            <SignUpmodel show={showSignUpModal} onHide={toggleSignUpModal} /> {/* 회원가입 모달 */}
             <header>
                 <Navbar bg="light" expand="lg">
                     <Container>
@@ -20,15 +45,18 @@ const Header = () => {
                             <img src={valanse_logo} alt="노이즈 로고" />
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">  {/*회원가입과 로그인 버튼을 오른쪽으로 정렬하는 거에요*/}
+                        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                             <Nav className="ml-auto">
-                                <Nav.Link>
-                                    <Button variant="primary">미정</Button>
-                                </Nav.Link>
-                                <Nav.Link>
-                                    <Button variant="secondary"
-                                        onClick={() => setSignUpModalOn(true)}>로그인</Button> {/*여기서 true는 회원가입 버튼 눌렀을 때 true되어서 회원가입 팝업창이 뜸*/}
-                                </Nav.Link>
+                                {/* 로그인 또는 로그아웃 버튼 */}
+                                {isLoggedIn ? (
+                                    <Nav.Link>
+                                        <Button variant="secondary" onClick={handleLogout}>로그아웃</Button>
+                                    </Nav.Link>
+                                ) : (
+                                    <Nav.Link>
+                                        <Button variant="secondary" onClick={toggleSignUpModal}>로그인</Button>
+                                    </Nav.Link>
+                                )}
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
--- blog/src/modal/SignUpmodel.js
+++ blog/src/modal/SignUpmodel.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Modal, Button, Form, Container } from 'react-bootstrap'
-import KaKaoimage from "../layouts/img/Kakao_login.png"
-import Googleimage from "../layouts/img/Goolge_login.png"
-import Naverimage from "../layouts/img/Naver_login.png"
+import React from 'react';
+import { Modal, Button, Container } from 'react-bootstrap';
+import KaKaoimage from "../layouts/img/Kakao_login.png";
+import Googleimage from "../layouts/img/Goolge_login.png";
+import Naverimage from "../layouts/img/Naver_login.png";
 
 const SignUpmodel = ({ show, onHide }) => {
-  const handleSignUpButtonClick = () => {
-    console.log('회원가입 페이지로 이동');
+  const handleKakaoLogin = () => {
+    // 카카오톡 인증 페이지로 이동하는 로직을 여기에 추가
+    window.location.href = "http://54.180.170.88:8080/oauth2/authorization/kakao";
   };
 
   return (
@@ -24,18 +25,20 @@ const SignUpmodel = ({ show, onHide }) => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          
           {/* 구글 로그인 버튼 */}
-          <Button variant="light" onClick={handleSignUpButtonClick}>
+          <Button variant="light">
             <img src={Googleimage} alt="구글 이미지" style={{ width: '170px', height: '45px' }} />
           </Button>
           {/* 카카오 로그인 버튼 */}
-          <Button variant="light" onClick={handleSignUpButtonClick}>
+          <Button variant="light" onClick={handleKakaoLogin}>
             <img src={KaKaoimage} alt="카카오톡 이미지" />
           </Button>
-          <Button variant="light" onClick={handleSignUpButtonClick}>
-            <img src={Naverimage} alt="네이버 이미지" style={{ width: '170px', height: '45px' }} />
-          </Button>
+          {/* 네이버 로그인 버튼 */}
+          <a href="/oauth2/authorization/naver">
+            <Button variant="light">
+              <img src={Naverimage} alt="네이버 이미지" style={{ width: '170px', height: '45px' }} />
+            </Button>
+          </a>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={onHide}>닫기</Button>
