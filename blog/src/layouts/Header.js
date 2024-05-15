2a7f7976923b35b6ab1a493dd099ac622ca477b5
unknown <thcodbs@naver.com>
2024-05-15 22:15:15 +0900

    액세스토큰발급재요청13

--- blog/src/layouts/Header.js
+++ blog/src/layouts/Header.js
@@ -33,7 +33,7 @@ const Header = () => {
     const getAccessToken = async () => {
         try {
             const response = await axios.post('http://54.180.170.88:8080/token/get', null, {
-                params: {
+                header: {
                     stateToken: stateToken
                 }
             });
