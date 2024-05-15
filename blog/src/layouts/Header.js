26efd6a23a01d22b8ae31d195afed25034728a58
unknown <thcodbs@naver.com>
2024-05-15 21:02:36 +0900

    액세스토큰발급재요청2

--- blog/src/layouts/Header.js
+++ blog/src/layouts/Header.js
@@ -4,56 +4,53 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import valanse_logo from "./img/valanse_logo.png";
-import { Link } from 'react-router-dom'; // Link 추가
-import axios from 'axios'; // axios 추가
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import SignUpmodel from "../modal/SignUpmodel";
-import Cookies from 'js-cookie'; // js-cookie 패키지 import
+import Cookies from 'js-cookie';
 
 const Header = () => {
-    const [showSignUpModal, setShowSignUpModal] = useState(false); // 회원가입 모달의 표시 여부를 관리하는 상태
-    const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 관리하는 상태
-    const [stateToken, setStateToken] = useState(''); // 상태 토큰을 관리하는 상태
-    const [accessToken, setAccessToken] = useState(''); // 액세스 토큰을 관리하는 상태
+    const [showSignUpModal, setShowSignUpModal] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [stateToken, setStateToken] = useState('');
+    const [accessToken, setAccessToken] = useState('');
 
     useEffect(() => {
-        // 쿠키에서 액세스 토큰 가져오기
         const accessTokenCookie = Cookies.get('access_token');
-        // 액세스 토큰이 존재하는 경우 로그인 상태 설정
         setIsLoggedIn(accessTokenCookie ? true : false);
 
-        // URL에서 상태 토큰 추출
         const urlParams = new URLSearchParams(window.location.search);
         const token = urlParams.get('stateToken');
         setStateToken(token);
-
-        // 액세스 토큰 가져오기
-        getAccessToken();
     }, []);
 
-    // 액세스 토큰 가져오는 함수
-    const getAccessToken = async () => {
-        try {
-            const response = await axios.post('http://54.180.170.88:8080/token/get', {}, {
-                headers: {
-                    'stateToken': stateToken
+    useEffect(() => {
+        const getAccessToken = async () => {
+            try {
+                const response = await axios.post('http://54.180.170.88:8080/token/get', {}, {
+                    headers: {
+                        'stateToken': stateToken
+                    }
+                });
+                if (response.status === 200) {
+                    setAccessToken(response.data.data);
+                } else {
+                    throw new Error('Failed to get access token');
                 }
-            });
-            if (response.status === 200) {
-                setAccessToken(response.data.data);
-            } else {
-                throw new Error('Failed to get access token');
+            } catch (error) {
+                console.error('Error getting access token:', error.message);
             }
-        } catch (error) {
-            console.error('Error getting access token:', error.message);
-        }
-    };
+        };
 
-    // 로그아웃 함수
+        // stateToken이 변경될 때마다 액세스 토큰 가져오기
+        if (stateToken) {
+            getAccessToken();
+        }
+    }, [stateToken]);
+
     const handleLogout = async () => {
         try {
-            // 로그아웃 요청 보내기
             await axios.post('https://54.180.170.88:8080/token/logout');
-            // 로그아웃 성공 시 쿠키 삭제 및 로그인 상태 변경
             Cookies.remove('access_token');
             setIsLoggedIn(false);
         } catch (error) {
@@ -61,7 +58,6 @@ const Header = () => {
         }
     };
 
-    // 로그인 모달을 열거나 닫는 함수
     const toggleSignUpModal = () => {
         setShowSignUpModal(!showSignUpModal);
     };
@@ -77,7 +73,6 @@ const Header = () => {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                             <Nav className="ml-auto">
-                                {/* 로그인 또는 로그아웃 버튼 */}
                                 {isLoggedIn ? (
                                     <Nav.Link>
                                         <Button variant="secondary" onClick={handleLogout}>로그아웃</Button>
@@ -87,20 +82,17 @@ const Header = () => {
                                         <Button variant="secondary" onClick={toggleSignUpModal}>로그인</Button>
                                     </Nav.Link>
                                 )}
-                                {/* 로그인 모달 */}
                                 <SignUpmodel show={showSignUpModal} onHide={toggleSignUpModal} />
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </header>
-            {/* 상태 토큰 출력 */}
             {stateToken && (
                 <div style={{ textAlign: 'center', margin: '20px' }}>
                     <h3>State Token: {stateToken}</h3>
                 </div>
             )}
-            {/* 액세스 토큰 출력 */}
             {accessToken && (
                 <div style={{ textAlign: 'center', margin: '20px' }}>
                     <h3>Access Token: {accessToken}</h3>
