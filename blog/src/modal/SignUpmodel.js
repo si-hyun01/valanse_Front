19148541c5c390e9e1a0d13491f9a44aad42160c
unknown <thcodbs@naver.com>
2024-05-15 20:44:28 +0900

    액세스토큰발급요청8

--- blog/src/modal/SignUpmodel.js
+++ blog/src/modal/SignUpmodel.js
@@ -1,49 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Modal, Button, Container } from 'react-bootstrap';
 import KaKaoimage from "../layouts/img/Kakao_login.png";
 import Googleimage from "../layouts/img/Google_login1.png";
 import Naverimage from "../layouts/img/Naver_login.png";
-import Cookies from 'js-cookie';
-import axios from 'axios'; // axios 패키지 import
 
 const SignUpmodel = ({ show, onHide }) => {
-  const [stateToken, setStateToken] = useState('');
-  const [accessToken, setAccessToken] = useState('');
-
-  useEffect(() => {
-    // URL에서 상태 토큰 추출
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get('stateToken');
-    setStateToken(token);
-  }, []);
-
-  const getAccessToken = async () => {
-    try {
-      const response = await axios.post('http://54.180.170.88:8080/token/get', {}, {
-        headers: {
-          'stateToken': stateToken
-        }
-      });
-      if (response.status === 200) {
-        setAccessToken(response.data.data);
-      } else {
-        throw new Error('Failed to get access token');
-      }
-    } catch (error) {
-      console.error('Error getting access token:', error.message);
-    }
-  };
-
-  const handleLogin = async () => {
-    await getAccessToken();
-    if (accessToken) {
-      Cookies.set('access_token', accessToken, { expires: 1 });
-      onHide();
-    } else {
-      console.error('Failed to get access token');
-    }
-  };
-
   const handleKakaoLogin = () => {
     window.location.href = "http://54.180.170.88:8080/oauth2/authorization/kakao";
   };
@@ -83,7 +44,6 @@ const SignUpmodel = ({ show, onHide }) => {
           </Button>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={handleLogin}>로그인</Button>
           <Button onClick={onHide}>닫기</Button>
         </Modal.Footer>
       </Container>
