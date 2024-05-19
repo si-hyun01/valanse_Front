789361630eb22b7d660df10b34c459ee76e331fc
unknown <thcodbs@naver.com>
2024-05-20 00:16:59 +0900

    문제조회완료1

--- blog/src/Tabs/Category/Food_ploblem.js
+++ blog/src/Tabs/Category/Food_ploblem.js
@@ -54,67 +54,69 @@ function FoodProblem() {
   }
 
   return (
-    <Container maxWidth="lg">
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12} style={{ height: '30px' }} />
-        <Grid item xs={12}>
-          <Typography variant="h4" align="center">{quizData.content}</Typography>
+    <Card variant="outlined" sx={{ padding: '20px' }}>
+      <Container maxWidth="lg">
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12} style={{ height: '30px' }} />
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center">{quizData.content}</Typography>
+          </Grid>
+          <Grid item xs={12} textAlign="center">
+            <IconButton onClick={handleOptionLike}>
+              <ThumbUpIcon /> {likes}
+            </IconButton>
+            <IconButton onClick={handleOptionDislike}>
+              <ThumbDownIcon /> {dislikes}
+            </IconButton>
+          </Grid>
+          <Grid item xs={6} textAlign="center">
+            <Card onClick={() => handleOptionSelect('A')}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image={quizData.imageA}
+                  alt="Option A Image"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {quizData.optionA}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {quizData.descriptionA}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={6} textAlign="center">
+            <Card onClick={() => handleOptionSelect('B')}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image={quizData.imageB}
+                  alt="Option B Image"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {quizData.optionB}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {quizData.descriptionB}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} textAlign="center">
+            <Button variant="contained" color="primary" onClick={handleNext} disabled={!selectedOption}>
+              Next
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12} textAlign="center">
-          <IconButton onClick={handleOptionLike}>
-            <ThumbUpIcon /> {likes}
-          </IconButton>
-          <IconButton onClick={handleOptionDislike}>
-            <ThumbDownIcon /> {dislikes}
-          </IconButton>
-        </Grid>
-        <Grid item xs={6} textAlign="center"> {/* 수정된 부분 */}
-          <Card sx={{ maxWidth: 500 }} onClick={() => handleOptionSelect('A')}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="300"
-                image={quizData.imageA}
-                alt="Option A Image"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {quizData.optionA}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {quizData.descriptionA}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6} textAlign="center"> {/* 수정된 부분 */}
-          <Card sx={{ maxWidth: 500 }} onClick={() => handleOptionSelect('B')}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="300"
-                image={quizData.imageB}
-                alt="Option B Image"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {quizData.optionB}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {quizData.descriptionB}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={12} textAlign="center">
-          <Button variant="contained" color="primary" onClick={handleNext} disabled={!selectedOption}>
-            Next
-          </Button>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Card>
   );
 }
 
