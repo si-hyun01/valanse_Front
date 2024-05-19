8ff42452fd12db2d101e24ac66cd37c74e5e74f1
unknown <thcodbs@naver.com>
2024-05-19 22:12:04 +0900

    로그아웃구현7

--- blog/src/layouts/Header.js
+++ blog/src/layouts/Header.js
@@ -51,7 +51,7 @@ const Header = () => {
             if (accessTokenCookie) {
                 await axios.post('https://valanse.site/token/logout', null, {
                     headers: {
-                        'Authorization': `Bearer ${accessTokenCookie}`,
+                        'Authorization': accessTokenCookie,
                         'Content-Type': 'application/json'
                     }
                 });
