d2844f19d44eb76149d1f21bc60a43aa5c513eb5
unknown <thcodbs@naver.com>
2024-05-20 14:10:44 +0900

    라우터생성3

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import NoticeBoard from '../src/Tabs/notinoti';
+import NoticeWrite from '../src/Tabs/noti_write';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/notice" element={<Layout />} />
         <Route path="/write-notice" element={<Layout />} />
         <Route path="/popularity" element={<Layout />} />
+        <Route path="/noti_board" element={<NoticeBoard />} />
+        <Route path="/noti_write" element={<NoticeWrite />} />
       </Routes>
     </Router>
   );
