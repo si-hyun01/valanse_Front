81a1cc3ae14d0320b08afd41afbb772cc0e8aef4
unknown <thcodbs@naver.com>
2024-05-23 22:36:18 +0900

    URL조정4

--- blog/src/layouts/Header.js
+++ blog/src/layouts/Header.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import valanse_logo from "./img/valanse_logo.png";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; // useNavigate를 추가합니다.
 import axios from 'axios';
 import SignUpmodel from "../modal/SignUpmodel";
 import Cookies from 'js-cookie';
@@ -14,7 +14,7 @@ const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false); 
     const [stateToken, setStateToken] = useState('');
     const [accessToken, setAccessToken] = useState('');
-    const navigate = useNavigate();
+    const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수를 가져옵니다.
 
     useEffect(() => {
         const accessTokenCookie = Cookies.get('access_token');
@@ -28,11 +28,12 @@ const Header = () => {
             getAccessToken(token);
         }
 
+        // 페이지가 처음 로드될 때만 인터셉터를 설정합니다.
         const interceptor = axios.interceptors.request.use(
             config => {
                 const token = Cookies.get('access_token');
                 if (token) {
-                    config.headers['Authorization'] = `Bearer ${token}`;
+                    config.headers['Authorization'] = token;
                 }
                 return config;
             },
@@ -41,6 +42,7 @@ const Header = () => {
             }
         );
 
+        // 컴포넌트가 언마운트될 때 인터셉터를 제거합니다.
         return () => {
             axios.interceptors.request.eject(interceptor);
         };
@@ -58,7 +60,7 @@ const Header = () => {
             setAccessToken(token);
             Cookies.set('access_token', token);
             setIsLoggedIn(true);
-            navigate('/', { replace: true }); // URL에서 stateToken 제거
+            window.location.replace('https://valanse.vercel.app/'); // URL에서 상태 토큰 제거 및 리다이렉트
         } catch (error) {
             console.error('Error getting access token:', error.message);
         }
@@ -89,6 +91,7 @@ const Header = () => {
         setShowSignUpModal(!showSignUpModal);
     };
 
+    // 로고 클릭 시 '전체' 탭으로 이동하는 함수
     const handleLogoClick = () => {
         window.location.href = 'https://valanse.vercel.app/';
     };
@@ -98,6 +101,7 @@ const Header = () => {
             <header>
                 <Navbar bg="light" expand="lg">
                     <Container>
+                        {/* 로고 클릭 이벤트 추가 */}
                         <Navbar.Brand onClick={handleLogoClick}>
                             <img src={valanse_logo} alt="Valanse Logo" style={{ cursor: 'pointer' }} />
                         </Navbar.Brand>
