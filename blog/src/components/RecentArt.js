dfa5d0c86289cfa85839c21dc9e46e329da6e47c
unknown <thcodbs@naver.com>
2024-05-26 23:32:53 +0900

    최근작4

--- blog/src/components/RecentArt.js
+++ blog/src/components/RecentArt.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const MyQuizzesPage = () => {
-  const [quizzes, setQuizzes] = useState([]);
+  const [recentQuizzes, setRecentQuizzes] = useState([]);
 
-  // 서버에서 퀴즈 데이터를 가져오는 함수
-  const fetchQuizzes = async () => {
+  // 서버에서 최근에 만들어진 퀴즈 데이터를 가져오는 함수
+  const fetchRecentQuizzes = async () => {
     try {
       const accessToken = Cookies.get('access_token'); // 쿠키에서 액세스 토큰 가져오기
       const response = await axios.get('https://valanse.site/quiz/sorted-by-created-at', {
@@ -14,23 +14,25 @@ const MyQuizzesPage = () => {
           'Authorization': accessToken // 헤더에 액세스 토큰 추가
         }
       });
-      setQuizzes(response.data.data); // 가져온 데이터의 data 필드를 상태에 저장
+      // 최근에 만들어진 퀴즈 중에서 최신 3개만 선택하여 상태에 저장
+      const recentQuizzesData = response.data.data.slice(0, 3);
+      setRecentQuizzes(recentQuizzesData);
     } catch (error) {
-      console.error('Error fetching quizzes:', error);
+      console.error('Error fetching recent quizzes:', error);
     }
   };
 
-  // 컴포넌트가 마운트될 때 퀴즈 데이터를 가져옴
+  // 컴포넌트가 마운트될 때 최근 퀴즈 데이터를 가져옴
   useEffect(() => {
-    fetchQuizzes();
+    fetchRecentQuizzes();
   }, []);
 
   return (
     <div style={{ marginTop: '20px', color: 'white' }}>
       <h2 style={{ color: 'white' }}>최근 작품</h2>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {/* 상태에 저장된 퀴즈 데이터를 순회하며 사진과 제목을 표시 */}
-        {quizzes.map((quiz, index) => (
+        {/* 상태에 저장된 최근 퀴즈 데이터를 순회하며 사진과 제목을 표시 */}
+        {recentQuizzes.map((quiz, index) => (
           <div key={index} style={{ width: 'calc(33.33% - 20px)', margin: '10px', border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }}>
             <img src={quiz.imageA} alt={quiz.content} style={{ width: '100%', height: 'auto' }} /> {/* 퀴즈의 imageA를 가져옴 */}
             <h3 style={{ marginTop: '10px', marginBottom: '5px', color: 'white' }}>{quiz.content}</h3> {/* 퀴즈의 content를 가져옴 */}
