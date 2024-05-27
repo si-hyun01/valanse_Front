7a1b72b5c086ce3180ee4e83ae91fd3fbb282242
unknown <thcodbs@naver.com>
2024-05-28 00:41:28 +0900

    메인페이지 개선

--- blog/src/Tabs/popularity.js
+++ blog/src/Tabs/popularity.js
@@ -13,7 +13,7 @@ const Popularity = () => {
     const fetchData = async () => {
         try {
             const response = await axios.get('https://valanse.site/quiz/sort-by-preference');
-            setQuizData(response.data.data); // 변경된 부분: response.data.data로 변경
+            setQuizData(response.data.data.slice(0, 3)); // 상위 3개 퀴즈만 설정
             setIsLoading(false);
         } catch (error) {
             console.error('Error fetching quiz data:', error.message);
