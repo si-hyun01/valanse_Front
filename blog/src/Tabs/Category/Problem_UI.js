7544c2dcd96493d24ccdfc108af34e70dfc8587d
unknown <thcodbs@naver.com>
2024-05-27 15:52:29 +0900

    댓글3

--- blog/src/Tabs/Category/Problem_UI.js
+++ blog/src/Tabs/Category/Problem_UI.js
@@ -19,7 +19,7 @@ import {
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import saveUserAnswer from './saveUserAnswer';  // 새롭게 추가된 라인
+import saveUserAnswer from "../../components/comments";
 
 const LAST_QUIZ_ID = 20;
 
