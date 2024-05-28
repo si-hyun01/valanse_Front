dcf154d3a680e9e44d840ea778adef83a7d6226e
unknown <thcodbs@naver.com>
2024-05-28 13:37:05 +0900

    UI개선12

--- blog/src/components/PopularArt.js
+++ blog/src/components/PopularArt.js
@@ -22,7 +22,7 @@ const Popularity = () => {
             <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                 {quizData.map((item, index) => (
                     <div key={index} style={{ width: 'calc(33.33% - 20px)', margin: '10px', border: '1px solid #ccc', borderRadius: '5px', padding: '10px', overflow: 'hidden' }}>
-                        <img src={item.imageA} alt="Option A" style={{ width: '100%', height: 'auto', objectFit: 'cover', maxHeight: '200px', maxWidth: '200px' }} />
+                        <img src={item.imageA} alt="Option A" style={{ width: '100%', height: 'auto', objectFit: 'cover', maxHeight: '300px', maxWidth: '300px' }} />
                         <h3 style={{ marginTop: '10px', marginBottom: '5px', color: 'white' }}>{item.content}</h3>
                     </div>
                 ))}
