6b2892eaed2310d2b7e21d54944d4390171496f1
unknown <thcodbs@naver.com>
2024-05-28 22:21:44 +0900

    공지수정모드3

--- blog/src/Tabs/notinoti.js
+++ blog/src/Tabs/notinoti.js
@@ -145,8 +145,8 @@ const NoticeBoard = () => {
           'Content-Type': 'application/json;charset=UTF-8'
         }
       });
-      setNotices(notices.map(notice => (notice.noticeId === noticeId ? { ...notice, title, content } : notice)));
-      setSelectedNotice({ ...selectedNotice, title, content });
+      setNotices(notices.map(notice => (notice.noticeId === noticeId ? {notice, title, content } : notice)));
+      setSelectedNotice({ selectedNotice, title, content });
     } catch (error) {
       console.error('Error updating notice:', error);
       alert('Failed to update the notice. Please try again.');
@@ -183,7 +183,7 @@ const NoticeBoard = () => {
   }, []);
 
   return (
-    <Container maxWidth="md" className="notii" style={{ marginTop: '50px' }}>
+    <Container maxWidth="80%" className="notii" style={{ marginTop: '50px' }}>
       <Typography variant="h4" className="notii-title" style={{ fontWeight: 'bold', color: 'black', marginBottom: '10px' }}>
         공지게시판
       </Typography>
