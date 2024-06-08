2b67d0fceff823eebc57015f72b70d97c0b7ff5e
unknown <thcodbs@naver.com>
2024-06-08 20:57:55 +0900

    댓글컴포넌트33

--- blog/src/components/comments.js
+++ blog/src/components/comments.js
@@ -16,7 +16,7 @@ function CommentUI({ quizId }) {
       year: 'numeric', month: '2-digit', day: '2-digit',
       hour: '2-digit', minute: '2-digit', second: '2-digit',
       hour12: false,
-      timeZone: 'Asia/Seoul' // 한국 시간
+      timeZone: 'UTC' // UTC 시간
     };
     return new Intl.DateTimeFormat('ko-KR', options).format(date);
   };
