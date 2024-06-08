c115c40028fc1be59d35bb87a11749110a5b6f10
unknown <thcodbs@naver.com>
2024-06-09 00:16:44 +0900

    마이페이지작업18

--- blog/src/layouts/remake_problem.js
+++ blog/src/layouts/remake_problem.js
@@ -7,51 +7,30 @@ function EditQuestionPage({ quizId }) {
     const [question, setQuestion] = useState('');
     const [descriptionA, setDescriptionA] = useState('');
     const [descriptionB, setDescriptionB] = useState('');
-    const [imageA, setImageA] = useState(null);
-    const [imageB, setImageB] = useState(null);
+    const [imageA, setImageA] = useState('');
+    const [imageB, setImageB] = useState('');
     const [selectedCategory, setSelectedCategory] = useState('');
     const [openDialog, setOpenDialog] = useState(false);
 
-    // 이미지 URL 상태 초기화
-    const [imageUrlA, setImageUrlA] = useState('');
-    const [imageUrlB, setImageUrlB] = useState('');
-
-    // 기존 퀴즈 데이터 불러오기
-    useEffect(() => {
-        const fetchQuizData = async () => {
-            try {
-                const response = await axios.get(`https://valanse.site/quiz/${quizId}`);
-                const { content, descriptionA, descriptionB, category } = response.data;
-                setQuestion(content);
-                setDescriptionA(descriptionA);
-                setDescriptionB(descriptionB);
-                setSelectedCategory(category);
-            } catch (error) {
-                console.error('Error fetching quiz data:', error.response ? error.response.data : error.message);
-            }
-        };
-
-        fetchQuizData();
-    }, [quizId]);
-
     const handleEdit = async () => {
-        const formData = new FormData();
         const editedQuizData = {
-            content: question,
-            descriptionA,
-            descriptionB,
-            category: selectedCategory
+            quizRegisterDto: {
+                content: question,
+                optionA: descriptionA,
+                optionB: descriptionB,
+                descriptionA: descriptionA,
+                descriptionB: descriptionB,
+                category: [selectedCategory]
+            },
+            image_A: imageA,
+            image_B: imageB
         };
-
-        formData.append('quizData', new Blob([JSON.stringify(editedQuizData)], { type: 'application/json' }));
-        if (imageA) formData.append('imageA', imageA);
-        if (imageB) formData.append('imageB', imageB);
 
         try {
-            const response = await axios.patch(`https://valanse.site/quiz/${quizId}/edit`, formData, {
+            const response = await axios.patch(`https://valanse.site/quiz/${quizId}`, editedQuizData, {
                 headers: {
                     'Authorization': Cookies.get('access_token'),
-                    'Content-Type': 'multipart/form-data'
+                    'Content-Type': 'application/json'
                 }
             });
             console.log('Quiz edited successfully:', response.data);
@@ -68,7 +47,6 @@ function EditQuestionPage({ quizId }) {
     return (
         <Container>
             <Grid container spacing={2}>
-                {/* 퀴즈 수정 폼 */}
                 <Grid item xs={12}>
                     <TextField
                         fullWidth
@@ -94,23 +72,20 @@ function EditQuestionPage({ quizId }) {
                     />
                 </Grid>
                 <Grid item xs={6}>
-                    {/* 이미지 A 업로드 */}
                     <input
                         type="file"
                         accept="image/*"
-                        onChange={(e) => setImageA(e.target.files[0])}
+                        onChange={(e) => setImageA(e.target.value)}
                     />
                 </Grid>
                 <Grid item xs={6}>
-                    {/* 이미지 B 업로드 */}
                     <input
                         type="file"
                         accept="image/*"
-                        onChange={(e) => setImageB(e.target.files[0])}
+                        onChange={(e) => setImageB(e.target.value)}
                     />
                 </Grid>
                 <Grid item xs={12}>
-                    {/* 카테고리 선택 */}
                     <FormControl fullWidth>
                         <InputLabel>카테고리 선택</InputLabel>
                         <Select
@@ -128,11 +103,9 @@ function EditQuestionPage({ quizId }) {
                     </FormControl>
                 </Grid>
                 <Grid item xs={12}>
-                    {/* 수정 버튼 */}
                     <Button variant="contained" color="primary" onClick={handleEdit}>수정</Button>
                 </Grid>
             </Grid>
-            {/* 알림 다이얼로그 */}
             <Dialog open={openDialog} onClose={handleCloseDialog}>
                 <DialogTitle>알림</DialogTitle>
                 <DialogContent>퀴즈가 수정되었습니다!</DialogContent>
