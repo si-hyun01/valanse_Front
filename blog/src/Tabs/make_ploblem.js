872df73f088e40c318504f9ab5e53d91f34484d8
unknown <thcodbs@naver.com>
2024-06-08 23:49:34 +0900

    마이페이지작업15

--- blog/src/Tabs/make_ploblem.js
+++ blog/src/Tabs/make_ploblem.js
@@ -1,109 +1,221 @@
 import React, { useState } from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { Button, Container, Grid, TextField, Dialog, DialogTitle, DialogContent, DialogActions, FormControl, InputLabel, Select, MenuItem, Card } from '@mui/material';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import ImageUpload from './ImageUpload'; // 이미지 업로드 컴포넌트 import
 
-function EditQuestionDialog({ open, handleClose, quiz, handleEdit, selectedCategory, handleCategoryChange }) {
-    const [editedQuestion, setEditedQuestion] = useState(quiz?.content || '');
-    const [editedOptionA, setEditedOptionA] = useState(quiz && quiz.optionA ? quiz.optionA : '');
-    const [editedOptionB, setEditedOptionB] = useState(quiz && quiz.optionB ? quiz.optionB : '');
-    const [editedDescriptionA, setEditedDescriptionA] = useState(quiz && quiz.descriptionA ? quiz.descriptionA : '');
-    const [editedDescriptionB, setEditedDescriptionB] = useState(quiz && quiz.descriptionB ? quiz.descriptionB : '');
-    const [editedImageA, setEditedImageA] = useState(null); // 이미지 A 상태
-    const [editedImageB, setEditedImageB] = useState(null); // 이미지 B 상태
 
-    const handleEditQuestion = async () => {
-        const editedQuiz = {
-            ...quiz,
-            content: editedQuestion,
-            optionA: editedOptionA,
-            optionB: editedOptionB,
-            descriptionA: editedDescriptionA,
-            descriptionB: editedDescriptionB
+function CreateQuestionPage({ onCreate, selectedCategory }) {
+    const [question, setQuestion] = useState('');
+    const [story1, setStory1] = useState('');
+    const [story2, setStory2] = useState('');
+    const [story1Image, setStory1Image] = useState(null);
+    const [story2Image, setStory2Image] = useState(null);
+    const [openDialog, setOpenDialog] = useState(false);
+
+    // 이미지 URL 상태 초기화
+    const [story1ImageUrl, setStory1ImageUrl] = useState('');
+    const [story2ImageUrl, setStory2ImageUrl] = useState('');
+
+    const resetForm = () => {
+        setQuestion('');
+        setStory1('');
+        setStory2('');
+        setStory1Image(null);
+        setStory2Image(null);
+        // 이미지 URL 상태 초기화
+        setStory1ImageUrl('');
+        setStory2ImageUrl('');
+    };
+
+    const handleCreate = async () => {
+        const formData = new FormData();
+        const quizRegisterDto = {
+            content: question,
+            optionA: story1,
+            optionB: story2,
+            descriptionA: story1,
+            descriptionB: story2,
+            category: [selectedCategory]
         };
 
-        try {
-            // quizRegisterDto를 formData에 추가
-            const formData = new FormData();
-            formData.append('quizRegisterDto', JSON.stringify(editedQuiz));
-            if (editedImageA) formData.append('imageA', editedImageA);
-            if (editedImageB) formData.append('imageB', editedImageB);
+        formData.append('quizRegisterDto', new Blob([JSON.stringify(quizRegisterDto)], { type: 'application/json' }));
+        formData.append('image_A', story1Image);
+        formData.append('image_B', story2Image);
 
-            await axios.patch(`https://valanse.site/quiz/${quiz.quizId}`, formData, {
+        try {
+            const response = await axios.post('https://valanse.site/quiz/register', formData, {
                 headers: {
                     'Authorization': Cookies.get('access_token'),
                     'Content-Type': 'multipart/form-data'
                 }
             });
-            handleEdit(editedQuiz);
-            handleClose();
+            console.log('Quiz created successfully:', response.data);
+            setOpenDialog(true);
+            resetForm(); // Reset the form after successful creation
         } catch (error) {
-            console.error('Error editing quiz:', error.response ? error.response.data : error.message);
+            console.error('Error creating quiz:', error.response ? error.response.data : error.message);
         }
     };
 
-    return (
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>퀴즈 수정</DialogTitle>
-            <DialogContent>
-                <TextField
-                    fullWidth
-                    label="퀴즈 제목"
-                    value={editedQuestion}
-                    onChange={(e) => setEditedQuestion(e.target.value)}
-                />
-                <TextField
-                    fullWidth
-                    label="옵션 A"
-                    value={editedOptionA}
-                    onChange={(e) => setEditedOptionA(e.target.value)}
-                />
-                <TextField
-                    fullWidth
-                    label="옵션 A 설명"
-                    value={editedDescriptionA}
-                    onChange={(e) => setEditedDescriptionA(e.target.value)}
-                />
-                <ImageUpload setImage={setEditedImageA} />
-                <TextField
-                    fullWidth
-                    label="옵션 B"
-                    value={editedOptionB}
-                    onChange={(e) => setEditedOptionB(e.target.value)}
-                />
-                <TextField
-                    fullWidth
-                    label="옵션 B 설명"
-                    value={editedDescriptionB}
-                    onChange={(e) => setEditedDescriptionB(e.target.value)}
-                />
-                <ImageUpload setImage={setEditedImageB} />
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
 
-                {/* 카테고리 선택 */}
-                <FormControl fullWidth style={{ backgroundColor: 'gray', marginBottom: '30px' }}>
-                    <InputLabel style={{ color: 'white' }}>카테고리 선택</InputLabel>
-                    <Select
-                        value={selectedCategory}
-                        onChange={handleCategoryChange}
-                        label="카테고리 선택"
-                    >
-                        <MenuItem value="축구">축구</MenuItem>
-                        <MenuItem value="음식">음식</MenuItem>
-                        <MenuItem value="연애">연애</MenuItem>
-                        <MenuItem value="노래">노래</MenuItem>
-                        <MenuItem value="생존">생존</MenuItem>
-                        <MenuItem value="드라마&영화">드라마&영화</MenuItem>
-                        <MenuItem value="일상">일상</MenuItem>
-                    </Select>
-                </FormControl>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose}>취소</Button>
-                <Button onClick={handleEditQuestion} variant="contained" color="primary">수정</Button>
-            </DialogActions>
-        </Dialog>
+    return (
+        <Card style={{border: '2px solid transparent', background: 'black', boxShadow: '0 0 10px #00FF00'}}>
+            <Container >
+                <Grid container spacing={2} justifyContent="center">
+                    <Grid item xs={12} style={{ height: '30px' }} />
+                    <Grid item xs={12}>
+                        <TextField
+                            style={{ backgroundColor: 'gray' }}
+                            label="문제 제목을 작성해주세요"
+                            fullWidth
+                            multiline
+                            rows={4}
+                            value={question}
+                            onChange={(e) => setQuestion(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            style={{ backgroundColor: 'gray' }}
+                            label="아래 사진과 함께 상황을 설명해주세요"
+                            fullWidth
+                            multiline
+                            rows={4}
+                            value={story1}
+                            onChange={(e) => setStory1(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            style={{ backgroundColor: 'gray' }}
+                            label="아래 사진과 함께 상황을 설명해주세요"
+                            fullWidth
+                            multiline
+                            rows={4}
+                            value={story2}
+                            onChange={(e) => setStory2(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <ImageUpload setImage={setStory1Image} setUploadImgUrl={setStory1ImageUrl} />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <ImageUpload setImage={setStory2Image} setUploadImgUrl={setStory2ImageUrl} />
+                    </Grid>
+                    <Grid item xs={12} style={{ height: '30px' }} />
+                    <Grid item xs={12} textAlign="center">
+                        <Button variant="contained" color="success" onClick={handleCreate}>
+                            문제 생성
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} style={{ height: '30px' }} />
+                </Grid>
+                <Dialog open={openDialog} onClose={handleCloseDialog}>
+                    <DialogTitle>알림</DialogTitle>
+                    <DialogContent>
+                        문제가 생성되었습니다!
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseDialog} color="primary">
+                            확인
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </Container>
+        </Card>
     );
 }
 
-export default EditQuestionDialog;
+const ImageUpload = ({ setImage, setImageUrl }) => {
+    const [uploadImgUrl, setUploadImgUrl] = useState('');
+
+    const onchangeImageUpload = (e) => {
+        const { files } = e.target;
+        const uploadFile = files[0];
+        setImage(uploadFile);
+        const reader = new FileReader();
+        reader.readAsDataURL(uploadFile);
+        reader.onloadend = () => {
+            setUploadImgUrl(reader.result);
+            // 이미지 URL 설정
+            setImageUrl(reader.result);
+        };
+    };
+    return (
+        <Grid container alignItems="center" justifyContent="space-around" >
+            <Grid item>
+                <img src={uploadImgUrl || "https://via.placeholder.com/200x150"} alt="사진 업로드 해주세요" style={{ width: '200px', height: '150px' }} />
+            </Grid>
+            <Grid item>
+                <Button variant="contained" color="primary" component="label" startIcon={<CloudUploadIcon />}>
+                    이미지 선택
+                    <input
+                        type="file"
+                        accept="image/*"
+                        style={{ display: 'none' }}
+                        onChange={onchangeImageUpload}
+                    />
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
+
+function App() {
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    const handleCreateQuestion = (newQuestion) => {
+        console.log('New question created:', newQuestion);
+    };
+
+    const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Container
+                maxWidth="md"
+                sx={{
+                    border: '2px solid transparent',
+                    boxShadow: '0 0 10px #00FF00, 0 0 20px #00FF00, 0 0 40px #00FF00',
+                    borderRadius: '10px',
+                    padding: '20px',
+                    marginTop: '40px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Grid container spacing={2} justifyContent="center">
+                    <Grid item xs={12} style={{ height: '30px' }} />
+                    <Grid item xs={12}>
+                        <FormControl fullWidth style={{ backgroundColor: 'gray', marginBottom: '30px' }}>
+                            <InputLabel style={{ color: 'white' }}>카테고리 선택</InputLabel>
+                            <Select
+                                value={selectedCategory}
+                                onChange={handleCategoryChange}
+                                label="카테고리 선택"
+                            >
+                                <MenuItem value="축구">축구</MenuItem>
+                                <MenuItem value="음식">음식</MenuItem>
+                                <MenuItem value="연애">연애</MenuItem>
+                                <MenuItem value="노래">노래</MenuItem>
+                                <MenuItem value="생존">생존</MenuItem>
+                                <MenuItem value="드라마&영화">드라마&영화</MenuItem>
+                                <MenuItem value="일상">일상</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                </Grid>
+                {selectedCategory && <CreateQuestionPage onCreate={handleCreateQuestion} selectedCategory={selectedCategory} />}
+            </Container>
+        </div>
+    );
+}
+
+export default App;
