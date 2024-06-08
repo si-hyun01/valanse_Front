d251588b7fdc4a37770941a67bf7f115aee421fa
unknown <thcodbs@naver.com>
2024-06-08 20:51:00 +0900

    댓글컴포넌트32

--- blog/src/components/comments.js
+++ blog/src/components/comments.js
@@ -12,13 +12,12 @@ function CommentUI({ quizId }) {
 
   const formatDate = (createdAt) => {
     const date = new Date(createdAt);
-    const koreanDate = new Date(date.getTime() + (date.getTimezoneOffset() * 60000) + (9 * 3600000)); // 한국 시간으로 변환
-    const year = koreanDate.getFullYear();
-    const month = String(koreanDate.getMonth() + 1).padStart(2, '0');
-    const day = String(koreanDate.getDate()).padStart(2, '0');
-    const hour = String(koreanDate.getHours()).padStart(2, '0');
-    const minute = String(koreanDate.getMinutes()).padStart(2, '0');
-    const second = String(koreanDate.getSeconds()).padStart(2, '0');
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
     return `${year}. ${month}. ${day}. ${hour}:${minute}:${second}`;
   };
 
