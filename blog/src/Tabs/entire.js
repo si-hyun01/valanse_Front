52b472e7ff0de065aae274bc52e9a6c26ce1b17f
unknown <thcodbs@naver.com>
2024-06-10 20:22:51 +0900

    수정중1

--- blog/src/Tabs/entire.js
+++ blog/src/Tabs/entire.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Grid, Container } from '@mui/material'; // 또는 사용 중인 UI 라이브러리의 Grid, Container 컴포넌트를 import
+import { Grid, Container } from '@mui/material';
 import SliderContainer from "../layouts/SliderContainer";
 import Popularity from "../Tabs/rank_randering";
 import RecentArt from "../components/RecentArt";
-//import PopularArt from "../components/PopularArt";
 
 const EnterPage = () => (
   <Grid container spacing={1} alignItems="center">
@@ -15,6 +14,11 @@ const EnterPage = () => (
     <Grid item xs={12}>
       <Container style={{ maxWidth: '80%' }}>
           <RecentArt />
+      </Container>
+    </Grid>
+    <Grid item xs={12}>
+      <Container style={{ maxWidth: '80%' }}> 
+          <Popularity />
       </Container>
     </Grid>
   </Grid>
