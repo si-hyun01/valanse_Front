0522571c1da4d84809f77d21251dbc32f2da479b
unknown <thcodbs@naver.com>
2024-06-13 16:05:50 +0900

    테스트11

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,8 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import NoticeBoard from '../src/Tabs/notinoti';
-import NoticeWrite from '../src/Tabs/noti_write';
+import NoticeWrite from '../src/Tabs/noti_write'; // NoticeWrite 컴포넌트 가져오기
 import Mypage from '../src/layouts/Mypage';
 import Header from '../src/layouts/Header';
 import MakeProblem from '../src/Tabs/make_ploblem';
@@ -15,11 +14,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Layout activeTab="전체" />} />
-        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/mypage" element={<Header />} />
         <Route path="/category/:categoryName" element={<Layout />} />
         <Route path="/problems/:categoryName" element={<ProblemUI />} />
         <Route path="/notice" element={<Layout />} />
-        <Route path="/write-notice" element={<Layout />} />
+        <Route path="/noti_write" element={<NoticeWrite />} /> {/* NoticeWrite 페이지로 연결되는 경로 추가 */}
         <Route path="/popularity" element={<Layout />} />
         <Route path="/problems" element={<Layout />} />
       </Routes>
