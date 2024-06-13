dc8d62c2ae8a01d454ac956a7b45bf61c6e0580f
unknown <thcodbs@naver.com>
2024-06-13 16:45:36 +0900

    테스트11

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import NoticeBoard from '../src/Tabs/notinoti';
-import NoticeWrite from '../src/Tabs/noti_write'; // NoticeWrite 컴포넌트 가져오기
-import Mypage from '../src/layouts/Mypage';
+import NoticeWrite from '../src/Tabs/noti_write';
+import MyPage from '../src/layouts/Mypage';
 import Header from '../src/layouts/Header';
 import MakeProblem from '../src/Tabs/make_ploblem';
 import ProblemUI from '../src/Tabs/Category/Problem_UI_Omaju';
@@ -14,11 +14,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Layout activeTab="전체" />} />
-        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/mypage" element={<Layout><MyPage /></Layout>} />
         <Route path="/category/:categoryName" element={<Layout />} />
         <Route path="/problems/:categoryName" element={<ProblemUI />} />
         <Route path="/notice" element={<Layout />} />
-        <Route path="/noti_write" element={<NoticeWrite />} /> {/* NoticeWrite 페이지로 연결되는 경로 추가 */}
+        <Route path="/noti_write" element={<NoticeWrite />} />
         <Route path="/popularity" element={<Layout />} />
         <Route path="/problems" element={<Layout />} />
       </Routes>
